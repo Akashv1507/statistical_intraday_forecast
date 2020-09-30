ba4e3743d8245d0ddfcb1743d3ccca16a87ae264
Akash Verma <akashv1507@gmai.com>
2020-10-01 02:08:13 +0530

    revsionwise error creation starts

--- index_revisionwiseErrorCreator.py
+++ index_revisionwiseErrorCreator.py
@@ -2,8 +2,6 @@ import argparse
 from datetime import datetime as dt
 from datetime import timedelta
 from src.appConfig import getAppConfigDict
-from src.blockwiseErrorStorage.blockwiseErrorCreator import createBlockwiseError
-
 
 configDict=getAppConfigDict()
 
@@ -12,16 +10,14 @@ startDate = endDate - timedelta(days=2)
 
 # startDate = dt.strptime("2020-08-25", '%Y-%m-%d')
 # endDate = dt.strptime("2020-09-08", '%Y-%m-%d')
-revisionNo = 'R0A'
 
-# get start, end dates and revision number from command line
+# get start, end dates from command line
 parser = argparse.ArgumentParser()
 parser.add_argument('--start_date', help="Enter Start date in yyyy-mm-dd format",
                     default=dt.strftime(startDate, '%Y-%m-%d'))
 parser.add_argument('--end_date', help="Enter end date in yyyy-mm-dd format",
                     default=dt.strftime(endDate, '%Y-%m-%d'))
-parser.add_argument('--revision_no', help="Enter revision no",
-                    default = revisionNo)
+
 
 args = parser.parse_args()
 startDate = dt.strptime(args.start_date, '%Y-%m-%d')
@@ -30,14 +26,14 @@ endDate = dt.strptime(args.end_date, '%Y-%m-%d')
 startDate = startDate.replace(hour=0, minute=0, second=0, microsecond=0)
 endDate = endDate.replace(hour=0, minute=0, second=0, microsecond=0)
 
-print('startDate = {0}, endDate = {1}, revisionNo = {2}'.format(dt.strftime(
-    startDate, '%Y-%m-%d'), dt.strftime(endDate, '%Y-%m-%d'), revisionNo))
+print('startDate = {0}, endDate = {1}'.format(dt.strftime(
+    startDate, '%Y-%m-%d'), dt.strftime(endDate, '%Y-%m-%d')))
 
 
-# create blockwise mw error and percentage mw error  between start and end dates
-isBlockwiseMwErrorCreationSuccess = createBlockwiseError(startDate, endDate, revisionNo, configDict)
+# create revisionwise mean abs error, RMSE, MAE, MAPE between start and end dates
+isRevisionwiseErrorCreationSuccess = createRevisionwiseError(startDate, endDate, configDict)
 
-if isBlockwiseMwErrorCreationSuccess:
-    print('blockwise mw error creation done...')
+if isRevisionwiseErrorCreationSuccess:
+    print('revisionwise error creation done...')
 else:
-    print('blockwise mw error creation failure...')
+    print('revisionwise error creation failure...')
